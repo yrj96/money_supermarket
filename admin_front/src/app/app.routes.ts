6205ec73bef2e74e1d8135412807288d9033e0ca
Andy.Yu <andy.yu@blockbi.com>
2018-04-03 17:47:11 +0800

    change project

--- admin_front/src/app/app.routes.ts
+++ admin_front/src/app/app.routes.ts
@@ -1,70 +1,23 @@
-import { RouterModule } from '@angular/router';
-
-import { AppComponent } from './app.component';
-import { UserLoginComponent } from './user/user-login/user-login.component';
-import { ForgetPwdComponent } from './user/forget-pwd/forget-pwd.component';
-import { UserRegisterComponent } from './user/user-register/user-register.component';
-import { ChartComponent } from './chart/chart.component';
-import { GaodeMapComponent } from './map/gaode-map/gaode-map.component';
-import { UseJqueryComponent } from './use-jquery/use-jquery.component';
-import { JsplumbDemoComponent } from './jsplumb-demo/jsplumb-demo.component';
-
-export const appRoutes=[
+import { LoginComponent } from './login/login.component';
+/**
+ * 这里是全局路由配置，全局路由只有2个，login和workspace，用户从login登录之后跳转到workspace
+ */
+export const appRoutes = [
 	{
-		path:'',
-		redirectTo:'posts',
-		pathMatch:'full'
+		path: '',
+		redirectTo: 'login',
+		pathMatch: 'full'
 	},
 	{
-		path:"echart",
-		component:ChartComponent
+		path: 'login',
+		component: LoginComponent
 	},
 	{
-		path:"map",
-		component:GaodeMapComponent
+		path: 'workspace',
+		loadChildren: './global-layout/workspace/workspace.module#WorkspaceModule'
 	},
 	{
-		path:'home',
-		loadChildren:'./home/home.module#HomeModule'
-	},
-	{
-		path:'posts',
-		loadChildren:'./home/home.module#HomeModule'
-	},
-	{
-		path:'post',
-		loadChildren:'./post/post.module#PostModule'
-	},
-	{
-		path:'login',
-		component:UserLoginComponent
-	},
-	{
-		path:'forgetpwd',
-		component:ForgetPwdComponent
-	},
-	{
-		path:'register',
-		component:UserRegisterComponent
-	},
-	{
-		path:'user',
-		loadChildren:'./user/user.module#UserModule'
-	},
-	{ 
-		path: 'manage', 
-		loadChildren:'./manage/manage.module#ManageModule'
-	},
-	{ 
-		path: 'jquery', 
-		component:UseJqueryComponent
-	},
-	{
-		path:'jsplumb',
-		component:JsplumbDemoComponent
-	},
-	{
-		path:'**',//fallback router must in the last
-		loadChildren:'./home/home.module#HomeModule'
+		path: '**', // fallback router must in the last
+		component: LoginComponent
 	}
 ];
--- admin_front/src/app/user/user-login/user-login.component.spec.ts
+++
@@ -1,8 +0,0 @@
-/* tslint:disable:no-unused-variable */
-
-import { TestBed, async } from '@angular/core/testing';
-import { UserLoginComponent } from './user-login.component';
-
-describe('Component: UserLogin', () => {
-	
-});
